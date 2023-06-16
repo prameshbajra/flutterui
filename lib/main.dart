50bc6886fe8ec13880b032456eca11069f82a025
prameshbajra <pe.messh@gmail.com>
2023-06-16 21:05:36 +0200

    Animation widget done.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,59 @@
 import 'package:flutter/material.dart';
-import 'package:flutterui/Homescreen.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MaterialApp(home: MyApp()));
+}
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 200),
+    )..repeat(reverse: true);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(), // HomeScreen
+    return Scaffold(
+      body: Center(
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            AnimatedBuilder(
+              animation: _controller,
+              builder: (_, child) {
+                return Container(
+                  width: 200 + (_controller.value * 100),
+                  height: 200 + (_controller.value * 100),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(color: Colors.blue, width: 3.0),
+                  ),
+                );
+              },
+            ),
+            FloatingActionButton(
+              onPressed: () {},
+              child: Icon(Icons.add),
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
